2d84fa2c3f91ac269c2293f09d705988618bcc0c
VijayDabkara155 <vijaydabkara@gmail.com>
2024-07-25 12:36:22 +0530

    Initial_commit

---
+++ database/migrations/2024_07_25_070002_create_plan_infos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plan_infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->decimal('range_from',10,2);
+            $table->decimal('range_to',10,2);
+            $table->decimal('return_per',3,1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plan_infos');
+    }
+};
